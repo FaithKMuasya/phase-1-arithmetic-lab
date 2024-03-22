f8afe91fd00f2248256b17b57226e10d50c48f71
Faith <loshyfaith@gmail.com>
2024-03-22 23:36:12 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1= 2;
+let num2= 31;
+let multiply= num1 * num2;
+let random= Math.ceil(Math.random() * 10);
+let num3= 11;
+let num4= 7;
+let mod= num3 % num4;
+let max= Math.max(20);
 
-// Write your code here
+
+
